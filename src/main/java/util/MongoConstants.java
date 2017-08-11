6d4a831e33d5fe0f4ef308c65d0254cf5a2b0da5
Nermin Boja <nboja1@etf.unsa.ba>
2017-08-11 23:06:51 +0200

    deploy

---
+++ src/main/java/util/MongoConstants.java
@@ -0,0 +1,24 @@
+package util;
+
+public class MongoConstants {
+
+    public static final String MONGO_USERNAME = "edit16";
+
+    public static final String MONGO_PASSWORD = "edit16";
+
+    public static final String MONGO_DATABASE = "heroku_2lctv2kc";
+
+    public static final String MONGO_SERVER = "ds013664.mlab.com";
+
+    public static final int MONGO_PORT = 13664;
+
+    public static final String USERS = "Users";
+
+    public static final String OWNERS = "Owners";
+
+    public static final String LOCKS = "Locks";
+
+    public static final String LOCATIONS = "Locations";
+
+    public static final String USER_LOCK = "UserLock" ;
+}
---
+++ target/CloudService/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<title>
+IoT WebService
+</title>
+<body>
+    <h1>Welcome to EdIT16</h1>
+    <img src="http://edit.world/wp-content/uploads/2016/01/post-sneak-peak-01-01.jpg">
+</body>
+</html>
